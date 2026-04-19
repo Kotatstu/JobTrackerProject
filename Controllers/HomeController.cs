db37b4b3169de021c3fa8e7099e19fc0b6243001
Kotatstu <kotatstu0303@gmail.com>
2026-04-19 17:52:49 +0700

    basic CRUD update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using JobTrackerProject.Models;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
 
 namespace JobTrackerProject.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly  AppDbContext _context;
+
+    //Constructor
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var list = _context.JobApplications.ToList();
+        return View(list);
     }
 
     public IActionResult Privacy()
@@ -20,5 +33,54 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    //GET: Show the view of create page
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST:
+    [HttpPost]
+    public async Task<IActionResult> Create(JobApplication job)
+    {
+        if(ModelState.IsValid)
+        {
+            await _context.JobApplications.AddAsync(job);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(job);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var job = await _context.JobApplications.FindAsync(id);
+
+        return View(job);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(JobApplication job)
+    {
+        _context.JobApplications.Update(job);
+        _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var job = await _context.JobApplications.FindAsync(id);
+        if(job == null)
+            return NotFound();
+
+        _context.JobApplications.Remove(job);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
     }
 }
